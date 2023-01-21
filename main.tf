a7e5e52c218a7a22321af43e1fa3036513901428
Silpashree <silpashree@bootlabstech.com>
2023-01-21 21:12:09 +0530

    initial

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "aws_ebs_volume" "ebsvolume" {
+  availability_zone = var.availability_zone
+  size              = var.size
+  type              = var.type
+  encrypted         = true
+  final_snapshot    = true
+}
---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "availability_zone" {
+  description = "value"
+  type        = string
+}
+variable "size" {
+  description = "value"
+  type        = string
+}
+variable "type" {
+  description = "value"
+  type        = string
+}
+variable "encrypted" {
+  description = "value"
+  type        = bool
+  default     = true
+}
+variable "final_snapshot" {
+  description = "value"
+  type        = string
+  default     = true
+}
+variable "region" {
+  description = "value"
+  type        = string
+}
+variable "role_arn" {
+  description = " The ARN of the IAM role"
+  type = string
+}
